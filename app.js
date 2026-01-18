b36103cb3aa6cb35bd41717fdd60303eb3df4be0
Karken Turarov <mr.turarov.karken@gmail.com>
2026-01-18 22:50:57 +0500

    removed first 2 lines with alerts

--- app.js
+++ app.js
@@ -21,8 +21,5 @@ function cachingDecorator(func) {
 
 slow = cachingDecorator(slow);
 
-alert( slow(1) ); // slow(1) кешируем
-alert( "Again: " + slow(1) ); // возвращаем из кеша
-
 alert( slow(2) ); // slow(2) кешируем
 alert( "Again: " + slow(2) ); // возвращаем из кеша
