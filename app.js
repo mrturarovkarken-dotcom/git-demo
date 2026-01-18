819a47e047cc70744837b88166a7a45bd9a308e6
Karken Turarov <mr.turarov.karken@gmail.com>
2026-01-18 16:32:27 +0500

    add function cashingDecorator

--- app.js
+++ app.js
@@ -3,3 +3,18 @@ function slow(x) {
   alert(`Called with ${x}`);
   return x;
 }
+
+function cachingDecorator(func) {
+  let cache = new Map();
+
+  return function(x) {
+    if (cache.has(x)) {    // если кеш содержит такой x,
+      return cache.get(x); // читаем из него результат
+    }
+
+    let result = func(x); // иначе, вызываем функцию
+
+    cache.set(x, result); // и кешируем (запоминаем) результат
+    return result;
+  };
+}
